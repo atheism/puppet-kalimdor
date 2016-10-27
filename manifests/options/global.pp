3a27c6cc0f50960b1fa243d0346d3f371237e6a4
Ning Yao <yaoning@unitedstack.com>
2016-10-27 15:03:10 +0800

    Add global.pp to initial global params in ceph.conf
    Signed-off-by: Ning Yao <yaoning@unitedstack.com>

--- manifests/options/global.pp
+++ manifests/options/global.pp
@@ -1,5 +1,42 @@
-class kalimdor::options::global {
+# Copyright 2016 (C) UnitedStack Inc.
+#
+# Author: Yao Ning <yaoning@unitedstack.com>
+#
+# == Class: kalimdor::params::global
+#
+# setting global parameters for each node
 
-  $global_options = {
+class kalimdor::options::global { 
+  $global_params = {
+    'fsid'                                 => undef,
+    'cluster_network'                      => undef,
+    'public_network'                       => undef,
+    'require_signatures'                   => false,
+    'cluster_require_signatures'           => false,
+    'service_require_signatures'           => false,
+    'sign_messages'                        => true,
+  }
+
+  $global_params_in_hiera = hiera('kalimdor::global') |$key_in_hiera| {"Key '${key_in_hiera}' not found"}
+  $global_params.each |$key, $val| {
+ 
+      $set_val = $global_params_in_hiera[$key]
+
+      if $set_val {
+
+          $really_val = $set_val
+      } else {
+
+          $really_val = $val
+      }
+
+      # set options in ceph.conf
+      if $really_val != '' { # undef automatically becomes an empty string, fix me if needed
+          ceph_config {
+              "global/${key}":   value => $really_val;
+          }
+      } else {
+          fail("global options ${key} must be set explicitly. undef value is not allowed")
+      }
   }
 }
